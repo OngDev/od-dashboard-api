0312b4702b4921f7514966e478574c171865de72
hunglm <hunglm-dev@gmail.com>
2021-03-23 22:24:35 +0700

    refactor code

--- src/main/java/com/ongdev/dashboard/DashboardApplication.java
+++ src/main/java/com/ongdev/dashboard/DashboardApplication.java
@@ -1,30 +1,11 @@
 package com.ongdev.dashboard;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ongdev.dashboard.models.YoutubeStatistic;
-import com.ongdev.dashboard.services.impl.YoutubeServiceImpl;
-import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DashboardApplication implements CommandLineRunner {
-	@Autowired
-	YoutubeServiceImpl youtubeService;
-	private final Logger logger = LoggerFactory.getLogger(DashboardApplication.class);
-
+public class DashboardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DashboardApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws JsonProcessingException {
-
-
 	}
 }
